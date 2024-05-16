338712100150c21b8c70b8ae3fc8a1a55319fed2
Jinlin28 <144567569+Jinlin28@users.noreply.github.com>
2024-05-16 21:05:11 +0200

    name changed

--- app/app.py
+++ app/app.py
@@ -3,9 +3,13 @@ import redis
 from flask import Flask, render_template, url_for
 import pandas as pd
 import matplotlib.pyplot as plt
+from decouple import config
 
 app = Flask(__name__)
-cache = redis.Redis(host='srv-captain--redis', port=6379)
+REDIS_HOST = config('REDIS_HOST') 
+REDIS_PASSWORD = config('REDIS_PASSWORD')
+cache = redis.Redis(host=REDIS_HOST, port=6379, password=REDIS_PASSWORD)
+
 
 def get_hit_count():
     retries = 5
